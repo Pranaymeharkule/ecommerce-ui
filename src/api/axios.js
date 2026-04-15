30b58169138935daf54f9d03a6db83c25d310c99
Pranay Meharkule <pranaymeharkule272005@gmail.com>
2026-04-15 13:16:55 +0530

    UPDATE-1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  bbaseURL:
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
-    : import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
